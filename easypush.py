800b0a56866ccd515739af4e1f6c7ba5fb4ea449
Leandro Medeiros <leandro.medeiros@serpro.gov.br>
2024-04-25 18:31:08 -0300

    Major refactoring and resolution for DNS names

--- easypush.py
+++ easypush.py
@@ -6,16 +6,49 @@ def send(hosts: list[str], port: int, message: str):
     Sends an UDP message to one or more hosts.
 
     Args:
-        hosts (list[str]): A list of host addresses to send the message to. They can be in either IPv4 or IPv6 format.
+        hosts (list[str]): A list of host addresses to send the message to. They can be names or IPs (IPv4 or IPv6).
         port (int): The port number to send the message to.
         message (str): The message to be sent.
     """
     for host in hosts:
-        family = socket.AF_INET if '.' in host else socket.AF_INET6
-        with socket.socket(family, socket.SOCK_DGRAM) as sock:
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # Enables broadcast
-            msg_bytes = bytes(message, 'utf-8')
-            sock.sendto(msg_bytes, (host, port))
+
+        # Checks if host is a domain name (has letters but it's not IPv6) and resolve it to an IP.
+        if ':' not in host and any(char.isalpha() for char in host):
+            try:
+                ip_addresses = [info[4][0] for info in socket.getaddrinfo(host, None)]
+                assert ip_addresses
+            except Exception:
+                raise ValueError(f'Could not resolve name: {host}')
+
+            if len(set(ip_addresses)) > 1:
+                print(f'Name {host} resolves to different IPs: {set(ip_addresses)}. Picking {ip_addresses[0]}...')
+
+            if ':' in ip_addresses[0]:
+                print(f'Host {host} resolved to an IPv6 address:', ip_addresses[0])
+
+            host = ip_addresses[0]
+
+        # Dinamically determine the protocol family (ipv4 or ipv6) based on the host address.
+        family = socket.AF_INET6 if ':' in host else socket.AF_INET
+
+        protocol = socket.SOCK_DGRAM  # socket.SOCK_STREAM for TCP
+        with socket.socket(family, protocol) as sock:
+            _send_udp(host, port, message, sock)
+
+
+def _send_udp(host: str, port: int, message: str, sock: socket.socket):
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # Enables broadcast
+    msg_bytes = bytes(message, 'utf-8')
+    sock.sendto(msg_bytes, (host, port))
+
+
+# def _send_tcp(host: str, port: int, message: str, sock: socket.socket):
+#     sock.connect((host, port))
+#     msg_bytes = bytes(message, 'utf-8')
+#     sock.sendall(msg_bytes)
+    # Receive a response from the server (optional)
+    # response = sock.recv(1024)  # Adjust buffer size as needed
+    # print("Received response:", response.decode('utf-8'))
 
 
 def listen(port: int, timeout: int = 0) -> str:
@@ -30,9 +63,24 @@ def listen(port: int, timeout: int = 0) -> str:
         str: The received message as a string.
     """
     with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) as sock:
-        sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)  # Enables both IPv6 and IPv4
-        if timeout:
-            sock.settimeout(timeout / 1000)
-        sock.bind(("::", port))
-        data, addr = sock.recvfrom(1024)
-        return data.decode('utf-8')
+        return _listen_udp(port, timeout, sock)
+
+
+def _listen_udp(port: int, timeout: int, sock: socket.socket) -> str:
+    sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)  # Enables both IPv6 and IPv4
+    if timeout:
+        sock.settimeout(timeout / 1000)
+    sock.bind(("::", port))
+    data, addr = sock.recvfrom(1024)
+    return data.decode('utf-8')
+
+
+# def _listen_tcp(port: int, timeout: int, sock: socket.socket) -> str:
+#     sock.bind(("localhost", port))  # Bind to localhost (modify if needed)
+#     sock.listen(1)  # Listen for incoming connections (queue length of 1)
+#     if timeout:
+#         sock.settimeout(timeout)  # Set timeout in seconds
+#     conn, addr = sock.accept()  # Wait for a connection
+#     with conn:  # Use context manager to ensure proper closing
+#         data = conn.recv(1024).decode('utf-8')
+#         return data
--- easypush_cli.py
+++ easypush_cli.py
@@ -2,6 +2,7 @@ import argparse
 import easypush
 import sys
 import signal
+import re
 from socket import timeout
 
 DESCRIPTION = \
@@ -13,14 +14,15 @@ Easypush can be used in two modes: sender and listener.
 
 - Send mode:
     To send messages, the first command must be 'send'.
-    The next parameter is for recipient(s) in 'IP@PORT' format.
-    There can be multiple IPs separated by comma.
-    IPs can be IPv4 or IPv6 and can also be bradcast IPs.
+    The next required argument is for recipient(s) as "HOSTS@PORT".
+    There can be multiple hosts separated by comma.
+    Hosts can be names or IPs (IPv4 or IPv6), including bradcast IPs.
     There can be only a single destination UDP port.
-    The third and last parameter is the text to be sent.
+    The third positional parameter is the text to be sent.
 
     Examples of using Easypush for sending messages:
     easypush send 192.168.0.10@1050 'Hello World!'
+    easypush send mypc.mydomain.net@1050 'Hello World!'
     easypush send 10.0.0.1,10.0.0.255@1050 'Hello Worlds!'
 
 - Listen mode:
@@ -28,7 +30,6 @@ Easypush can be used in two modes: sender and listener.
     It must be followed by the UDP port number to listen on.
     By default, easypush waits for a message and terminates.
     The optional '-t/--timeout' parameter limits the wait in millis.
-    The optional '-k/--keep-alive' flag is to keep listening.
 
     Examples of using Easypush for listening to messages:
     easypush listen 1050  # Get first message on port 1050 and exit.
@@ -38,9 +39,65 @@ Easypush can be used in two modes: sender and listener.
 ====================================================================
     '''
 
+RECIPIENTS_PATTERN = r'^([a-zA-Z0-9.,:-]+)@([\d]+)$'
+
 
 def out(msg: str):
-    print(f'[EASYPUSH] {msg}', flush=True)
+    print(f'[easypush] {msg}', flush=True)
+
+
+def parse_port(port: str) -> int:
+    if port and port.isdigit():
+        int_port = int(port)
+        if 0 <= int_port <= 65535:
+            return int_port
+    raise argparse.ArgumentTypeError('Invalid port: it must be a number from 0 to 65535')
+
+
+def parse_recipients(recipients: str) -> tuple[list[str], int]:
+    if (match := re.search(RECIPIENTS_PATTERN, recipients)):
+        hosts, port = match.group(1), match.group(2)
+        int_port = parse_port(port)
+        return hosts.split(','), int_port
+    raise argparse.ArgumentTypeError('Invalid recipients format. It should be "HOSTS@PORT". Try -h for details.')
+
+
+def send(args):
+    hosts, port = args.RECIPIENTS
+    for host in hosts:
+        try:
+            easypush.send([host], port, args.MESSAGE)
+            out(f'Message sent to {host} on port {port}.')
+        except Exception as e:
+            print(f'Error when sending to host {host}: {e}', file=sys.stderr)
+
+
+def listen(args):
+    def handle_shutdown(signal, frame):
+        out("Shutdown signaled! Terminating...")
+        sys.exit(0)
+
+    signal.signal(signal.SIGTERM, handle_shutdown)
+    out(f'Starting to listen on port {args.PORT_NUMBER}')
+
+    while True:
+        try:
+            msg = easypush.listen(args.PORT_NUMBER, args.timeout)
+            out('Received message: ' + msg)
+            if args.keep_alive:
+                continue
+        except KeyboardInterrupt:
+            out("Interrupted by user.")
+        except timeout:
+            out('Timeout reached for listener.')
+        except OSError as e:
+            if e.errno == 98:
+                out('Port seems to be already in use!')
+            else:
+                sys.stderr.write(f'{e}\n')
+        except Exception as e:
+            sys.stderr.write(f'{e}\n')
+        break
 
 
 def main():
@@ -48,58 +105,21 @@ def main():
     subparsers = parser.add_subparsers(dest='mode', required=True, title='mode command')
 
     sender_parser = subparsers.add_parser('send')
-    sender_parser.add_argument('RECIPIENTS', type=str, help='IP(s) and port of recipients in "IPs@PORT" format. Comma separated if multiple IPs.')
-    sender_parser.add_argument('MESSAGE', type=str, help='Text to be sent to the recipients.')
+    sender_parser.add_argument('RECIPIENTS', type=parse_recipients, help='hosts and port of recipients in "HOSTS@PORT" format. Comma separated if multiple hosts.')
+    sender_parser.add_argument('MESSAGE', type=str, help='text to be sent to the recipients.')
 
     listener_parser = subparsers.add_parser('listen')
-    listener_parser.add_argument('PORT_NUMBER', type=int, help='UDP port to listen on.')
-    listener_parser.add_argument('-t', '--timeout', type=int, default=0, help='Timeout for quitting, in milliseconds.')
-    listener_parser.add_argument('-k', '--keep-alive', action='store_true', help='Flag for not stopping at the first message, but to keep listening.')
+    listener_parser.add_argument('PORT_NUMBER', type=parse_port, help='UDP port to listen on.')
+    listener_parser.add_argument('-t', '--timeout', type=int, default=0, help='timeout for quitting, in milliseconds.')
+    listener_parser.add_argument('-k', '--keep-alive', action='store_true', help='flag for not stopping at the first message, but to keep listening.')
 
     args = parser.parse_args()
 
-    def parse_port(port: int | str | None) -> int:
-        int_port = port if isinstance(port, int) else None
-        if isinstance(port, str) and port.isdigit():
-            int_port = int(port)
-        elif not int_port or not (0 <= int_port <= 65535):
-            sys.stderr.write('Invalid port: it must be a number from 0 to 65535\n')
-            sys.exit(1)
-        assert isinstance(int_port, int) and 0 <= int_port <= 65535
-        return int_port
-
     if args.mode == 'send':
-        ips, _, port = args.RECIPIENTS.partition('@')
-        recipients = ips.split(',')
-        int_port = parse_port(port)
-        easypush.send(recipients, int_port, args.MESSAGE)
+        send(args)
 
     elif args.mode == 'listen':
-
-        def handle_shutdown(signal, frame):
-            out("Shutdown signaled! Terminating...")
-            sys.exit(0)
-
-        signal.signal(signal.SIGTERM, handle_shutdown)
-
-        out(f'Starting to listen on port {args.PORT_NUMBER}')
-
-        while True:
-            try:
-                msg = easypush.listen(parse_port(args.PORT_NUMBER), args.timeout)
-                out('Received message: ' + msg)
-                if args.keep_alive:
-                    continue
-            except KeyboardInterrupt:
-                out("Interrupted by user.")
-            except timeout:
-                out('Timeout reached for listener.')
-            except OSError as e:
-                if e.errno == 98:
-                    out('Port seems to be already in use!')
-                else:
-                    raise e
-            break
+        listen(args)
 
 
 if __name__ == "__main__":
